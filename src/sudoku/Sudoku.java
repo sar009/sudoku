aa16edd10c1cd519a86cfd807ea42d2a8329e442
sar009 <sarad.mohanan@gmail.com>
2013-05-10 12:55:59 +0530

    first commit

---
+++ src/sudoku/Sudoku.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sudoku;
+
+/**
+ *
+ * @author sarad
+ */
+public class Sudoku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+	Display d=new Display();
+	d.setVisible(true);
+    }
+}
